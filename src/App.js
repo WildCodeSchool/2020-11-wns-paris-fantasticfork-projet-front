ec7c463da8f8dd05e91c3a569e604615cbeec5fa
LenaSim <radical.rin@gmail.com>
2020-11-17 14:30:20 +0100

    Work structure, installation in common

--- src/App.js
+++ src/App.js
@@ -1,5 +1,28 @@
+import { Paper, Button, Icon } from '@material-ui/core';
+
 function App() {
-  return <div></div>;
+  return (
+    <div style={{ width: '95%' }}>
+      <Paper style={{ width: '100%', height: 50, margin: 20 }} elevation={3}>
+        <Button>
+          <Icon>add</Icon>
+        </Button>
+        article 1
+      </Paper>
+      <Paper style={{ width: '80%', height: 50, margin: 20 }} elevation={3}>
+        article 1
+      </Paper>
+      <Paper style={{ width: '100%', height: 50, margin: 20 }} elevation={3}>
+        article 1
+      </Paper>
+      <Paper style={{ width: '100%', height: 50, margin: 20 }} elevation={3}>
+        article 1
+      </Paper>
+      <Paper style={{ width: '100%', height: 50, margin: 20 }} elevation={3}>
+        article 1
+      </Paper>
+    </div>
+  );
 }
 
 export default App;

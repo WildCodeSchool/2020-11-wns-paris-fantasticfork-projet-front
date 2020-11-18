1843ea48e74dfb20c2969d4d8a7c738c1f8949f0
René-Marcel Zadi <rene.mzadi45@gmail.com>
2020-11-18 02:01:34 +0100

    first development on the article-list branch

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
-import { Paper, Button, Icon } from '@material-ui/core';
+import React from "react";
+import { Typography, Button } from "@material-ui/core";
+import ArticleList from "./Components/Forum/ArticleList.js";
+import Article from "./Components/Forum/Article";
+import Paper from "@material-ui/core/Paper";
+import "./index.css";
 
 function App() {
   return (
-    <div style={{ width: '95%' }}>
-      <Paper style={{ width: '100%', height: 50, margin: 20 }} elevation={3}>
-        <Button>
-          <Icon>add</Icon>
-        </Button>
-        article 1
-      </Paper>
-      <Paper style={{ width: '80%', height: 50, margin: 20 }} elevation={3}>
-        article 1
-      </Paper>
-      <Paper style={{ width: '100%', height: 50, margin: 20 }} elevation={3}>
-        article 1
-      </Paper>
-      <Paper style={{ width: '100%', height: 50, margin: 20 }} elevation={3}>
-        article 1
-      </Paper>
-      <Paper style={{ width: '100%', height: 50, margin: 20 }} elevation={3}>
-        article 1
-      </Paper>
+    <div style={{ width: "100%" }}>
+      <Typography variant="h2" component="h2" className="text1">
+        Studn'Connect
+      </Typography>
+      <Typography variant="h4" component="h4" className="text1">
+        Your Questions
+      </Typography>
+      <ArticleList />
+      <Paper />
+      <Button />
+      <Article />
     </div>
   );
 }

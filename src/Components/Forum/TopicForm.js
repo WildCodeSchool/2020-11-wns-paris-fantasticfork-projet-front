363afb5b2d0cd61e2b800b5e9e3d906b22eab484
clemderome <clementine.derome@gmail.com>
2020-11-19 11:25:14 +0100

    refacto code, and place addTags and addUrl

--- src/Components/Forum/TopicForm.js
+++ src/Components/Forum/TopicForm.js
@@ -1,69 +1,121 @@
 import React, { useState } from 'react';
-import { Button, Paper, TextField } from '@material-ui/core';
+import { Button, Paper, TextField, Link, Icon, IconButton } from '@material-ui/core';
 
 import './TopicForm.css';
 
 const TopicForm = () => {
     const [inputFields, setInputFields] = useState(
-        [{
+        {
+            username: 'Clem Stud',
             subject: '',
             message: '',
-            url: ''
-        }]
+            url: [],
+            tags: []
+        }
     )
+    const [newUrl, setNewUrl] = useState('')
 
-    const handleChange = (index, e) => {
-        const values = [...inputFields]
-        values[index][e.target.name] = e.target.value
-        setInputFields(values)
+    const addUrl = () => {
+        if (newUrl === '') { return null }
+        let url = Array.from(inputFields.url)
+        url.push(newUrl)
+        setNewUrl('')
+        setInputFields({ ...inputFields, url })
     }
+    const deleteUrl = (idx) => {
+        let url = Array.from(inputFields.url)
+        url.splice(idx, 1)
+        setInputFields({ ...inputFields, url })
+    }
+
+    const addTags = event => {
+        if (event.target.value === '') { return null }
+        let tags = Array.from(inputFields.tags)
+        tags.push(event.target.value)
+        event.target.value = "";
+        setInputFields({ ...inputFields, tags })
+    };
+
+    const removeTags = indexToRemove => {
+        let tags = Array.from(inputFields.tags)
+        tags.splice(indexToRemove, 1)
+        setInputFields({ ...inputFields, tags })
+    };
 
     const handleSubmit = (e) => {
         e.preventDefault();
+        console.log(inputFields)
     }
 
     return (
         <Paper>
             <h3>New Topic</h3>
-            <form onSubmit={handleSubmit}>
-                {inputFields.map((field, index) => (
-                    <div key={index} className="form">
-                        <TextField 
-                            id='title' 
-                            name='subject' 
-                            label='Subject' 
-                            variant='standard' 
-                            autoFocus={true} 
-                            value={inputFields.subject} 
-                            onChange={(e) => handleChange(index, e)}
-                            required  />
-                        <TextField 
-                            id='message' 
-                            name='message' 
-                            label='Message' 
-                            multiline={true} 
-                            variant='standard' 
-                            value={inputFields.message} 
-                            onChange={(e) => handleChange(index, e)}
-                            required />
-                        <TextField 
-                            id='url' 
-                            name="url" 
-                            label='URL' 
-                            variant='standard'
-                            value={inputFields.url} 
-                            onChange={(e) => handleChange(index, e)} />
-                        <Button
-                            className="submit-button"
-                            variant="contained"
-                            color="secondary"
-                            type="submit"
-                            onClick={handleSubmit} >
-                                Send
-                        </Button>
+
+            <div className="form">
+                <TextField
+                    id='title'
+                    name='subject'
+                    label='Subject'
+                    variant='standard'
+                    autoFocus={true}
+                    value={inputFields.subject}
+                    onChange={(e) => setInputFields({ ...inputFields, subject: e.target.value })}
+                    required />
+                <TextField
+                    id='message'
+                    name='message'
+                    label='Message'
+                    multiline={true}
+                    variant='standard'
+                    value={inputFields.message}
+                    onChange={(e) => setInputFields({ ...inputFields, message: e.target.value })}
+                    required />
+                {inputFields.url.length > 0 && inputFields.url.map((url, idx) => (
+                    <div key={url} className='url_list blue' href={url} target='_blank' rel='noopener noreferrer'>
+                        <Icon style={{ paddingRight: 10 }}>link</Icon>
+                        {url}
+                        <div style={{ flex: 1 }} />
+                        <IconButton aria-label="delete">
+                            <Icon fontSize="small" onClick={() => deleteUrl(idx)}> delete</Icon>
+                        </IconButton>
                     </div>
                 ))}
-            </form>
+                <div>
+                    <TextField
+                        id='url'
+                        name="url"
+                        label='URL'
+                        variant='standard'
+                        value={newUrl}
+                        onChange={(e) => setNewUrl(e.target.value)} />
+                    <button onClick={addUrl} className='add-url'>+</button>
+                </div>
+                <div className="tags-input">
+                    <ul id="tags">
+                        {inputFields.tags.length > 0 && inputFields.tags.map((tag, index) => (
+                            <li key={index} className="tag">
+                                <span className='tag-title'>{tag}</span>
+                                <span className='tag-close-icon'
+                                    onClick={() => removeTags(index)}
+                                >x</span>
+                            </li>
+                        ))}
+                    </ul>
+                    <input
+                        type="text"
+                        onKeyUp={event => event.key === "Enter" ? addTags(event) : null}
+                        placeholder="Press enter to add tags"
+                    />
+                </div>
+                <Button
+                    className="submit-button"
+                    variant="contained"
+                    style={{marginTop: "20px", marginBottom: "20px"}}
+                    color="secondary"
+                    onClick={handleSubmit} >
+                    Send
+                </Button>
+            </div>
         </Paper>
     )
 }

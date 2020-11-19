64f5bd22aa12e4e431843f77885143a502763eab
LenaSim <radical.rin@gmail.com>
2020-11-19 14:00:21 +0100

    test

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ function App() {
       </div>
       <div style={{ width: '100%' }}>
         <SubNavbar title={menu.text} />
-        <div style={{ marginTop: 70, padding: 40, paddingRight: 300 }}>
+        <div className='lightgreyBackground' style={{ marginTop: 70, padding: 40 }}>
           <Switch>
             <Route exact path='/' component={ArticleList} />
             <Route exact path='/topics' history component={ArticleList} />

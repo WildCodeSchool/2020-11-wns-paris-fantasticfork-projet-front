a36a8f966e229e66a738c5313ddddbfb0b4738d2
coneeone <nicolas.laudinet@sfr.fr>
2021-01-06 16:09:32 +0100

    added Apollo provider & client

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import { Switch, Route } from 'react-router-dom';
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
+import { gql } from '@apollo/client';
 import Navbar from './Components/Navbar/Navbar';
 import SubNavbar from './Components/Navbar/SubNavbar';
 import ArticleContainer from './Components/Forum/ArticleContainer';
@@ -12,23 +14,40 @@ const menu = [
   { text: 'Forum', icon: 'forum', link: '/topics' },
 ];
 
+const client = new ApolloClient({
+  uri: 'http://localhost:4000/graphql',
+  cache: new InMemoryCache()
+});
+
+// client.query({
+//   query: gql`
+//     query Topics {
+//       topics {
+//         username
+//       }
+//     }
+//   `
+// }).then(result => console.log(result));
+
 function App() {
   return (
-    <div className='flex_ lightgreyBackground' style={{ height: '100vh', width: '100%' }}>
-      <div style={{ width: 200 }}>
-        <Navbar menu={menu} />
-      </div>
-      <div style={{ width: '100%' }}>
-        <SubNavbar title={menu.text} />
-        <div className='lightgreyBackground' style={{ marginTop: 70, padding: 40 }}>
-          <Switch>
-            <Route exact path='/' component={ArticleList} />
-            <Route exact path='/topics' history component={ArticleList} />
-            <Route path='/topics/:id' component={ArticleContainer} />
-          </Switch>
+    <ApolloProvider client={client}>
+      <div className='flex_ lightgreyBackground' style={{ height: '100vh', width: '100%' }}>
+        <div style={{ width: 200 }}>
+          <Navbar menu={menu} />
+        </div>
+        <div style={{ width: '100%' }}>
+          <SubNavbar title={menu.text} />
+          <div className='lightgreyBackground' style={{ marginTop: 70, padding: 40 }}>
+            <Switch>
+              <Route exact path='/' component={ArticleList} />
+              <Route exact path='/topics' history component={ArticleList} />
+              <Route path='/topics/:id' component={ArticleContainer} />
+            </Switch>
+          </div>
         </div>
       </div>
-    </div>
+    </ApolloProvider>
   );
 }
 

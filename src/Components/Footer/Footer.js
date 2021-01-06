3ca4e3350a5be6891493d9c3b580fada238e09ec
clemderome <clementine.derome@gmail.com>
2021-01-06 13:44:27 +0100

    WIP

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,4 +1,8 @@
 import React from "react";
+import FacebookIcon from '@material-ui/icons/Facebook';
+import InstagramIcon from '@material-ui/icons/Instagram';
+import TwitterIcon from '@material-ui/icons/Twitter';
+
 import "./Footer.css";
 
 const Footer = () => {
@@ -8,6 +12,19 @@ const Footer = () => {
       <h4 className="h4-footer">
         © Stud-Connect, 2021. Fantastique-fork from WCS
         </h4>
+      <div className="footer-links">
+        <ul>
+          <li>Contact</li>
+          <li>About us</li>
+          <li>FAQ</li>
+        </ul>
+        <ul>
+          <li><FacebookIcon /></li>
+          <li><InstagramIcon /></li>
+          <li><TwitterIcon /></li>
+        </ul>
+      </div>
+
     </div>
   );
 };

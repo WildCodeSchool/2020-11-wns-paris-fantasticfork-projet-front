36f8032fc4fd762efcf335b74817b1b76d38c033
René-Marcel Zadi <rene.mzadi45@gmail.com>
2021-01-06 11:25:16 +0100

    starting footer

--- src/App.js
+++ src/App.js
@@ -1,31 +1,39 @@
-import { Switch, Route } from 'react-router-dom';
-import Navbar from './Components/Navbar/Navbar';
-import SubNavbar from './Components/Navbar/SubNavbar';
-import Article from './Components/Forum/Article';
-import ArticleList from './Components/Forum/ArticleList';
-import './App.css';
+import { Switch, Route } from "react-router-dom";
+import Navbar from "./Components/Navbar/Navbar";
+import SubNavbar from "./Components/Navbar/SubNavbar";
+import Article from "./Components/Forum/Article";
+import ArticleList from "./Components/Forum/ArticleList";
+import Footer from "./Components/Footer/Footer";
+import "./App.css";
 
 const menu = [
-  { text: 'Home', icon: 'home', link: '/' },
-  { text: 'Home', icon: 'home', link: '/' },
-  { text: 'Home', icon: 'home', link: '/' },
-  { text: 'Forum', icon: 'forum', link: '/topics' },
+  { text: "Home", icon: "home", link: "/" },
+  { text: "Home", icon: "home", link: "/" },
+  { text: "Home", icon: "home", link: "/" },
+  { text: "Forum", icon: "forum", link: "/topics" },
 ];
 
 function App() {
   return (
-    <div className='flex_ lightgreyBackground' style={{ height: '100vh', width: '100%' }}>
+    <div
+      className="flex_ lightgreyBackground"
+      style={{ height: "100vh", width: "100%" }}
+    >
       <div style={{ width: 200 }}>
         <Navbar menu={menu} />
       </div>
-      <div style={{ width: '100%' }}>
+      <div style={{ width: "100%" }}>
         <SubNavbar title={menu.text} />
-        <div className='lightgreyBackground' style={{ marginTop: 70, padding: 40 }}>
+        <div
+          className="lightgreyBackground"
+          style={{ marginTop: 70, padding: 40 }}
+        >
           <Switch>
-            <Route exact path='/' component={ArticleList} />
-            <Route exact path='/topics' history component={ArticleList} />
-            <Route path='/topics/:id' component={Article} />
+            <Route exact path="/" component={ArticleList} />
+            <Route exact path="/topics" history component={ArticleList} />
+            <Route path="/topics/:id" component={Article} />
           </Switch>
+          <Footer />
         </div>
       </div>
     </div>
---
+++ src/Components/Footer/Footer.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./Footer.css";
+
+const Footer = () => {
+  return (
+    <div className="Footer">
+      <div>
+        <h4 className="h4-footer">
+          © Stud-Connect, 2021. Fantastique-fork from WCS
+        </h4>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;

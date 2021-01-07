5709202ab016520e8e5675030e6935aff11aaa2e
LenaSim <radical.rin@gmail.com>
2021-01-07 19:10:06 +0100

    Update comment

--- src/Components/Forum/Comment.js
+++ src/Components/Forum/Comment.js
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { gql, useMutation } from '@apollo/client';
-import { Paper, Icon, Avatar, Chip, Typography, Button } from '@material-ui/core';
+import { Paper, Icon, Avatar, Chip, Typography, Button, TextField } from '@material-ui/core';
 import './Comment.css';
 
 // prettier-ignore
@@ -25,10 +25,25 @@ const UPDATE_COMMENT = gql`
 export default function Comment(props) {
   const { commentId, date, name, message, like, dislike, best, lastUpdateDate, refresh } = props;
   const [updateComment, { loading }] = useMutation(UPDATE_COMMENT);
+  const [editMode, setEditMode] = useState(false);
+  const [commentMessage, setCommentMessage] = useState(message);
 
-  const addLike = () => {
+  const updateLikeDislike = (mode) => {
     const likeNumber = like ? like + 1 : 1;
-    updateComment({ variables: { commentId: commentId, like: likeNumber } });
+    const dislikeNumber = dislike ? dislike + 1 : 1;
+    let variables;
+    if (mode === 'like') {
+      variables = { commentId: commentId, like: likeNumber };
+    } else {
+      variables = { commentId: commentId, dislike: dislikeNumber };
+    }
+    updateComment({ variables });
+    refresh();
+  };
+
+  const updateMessage = () => {
+    updateComment({ variables: { commentId: commentId, commentBody: commentMessage } });
+    setEditMode(false);
     refresh();
   };
 
@@ -45,10 +60,10 @@ export default function Comment(props) {
 
               <Typography variant='caption' className='lightgrey'>
                 Posted on
-                <span className='lightgrey'> {getDateFromTimestamp(date)}</span>
+                <span className='lightgrey'> {getDateFromTimestamp(date)} </span>
                 {lastUpdateDate && (
                   <>
-                    "Modified on"
+                    | Modified on
                     <span className='lightgrey'> {getDateFromTimestamp(lastUpdateDate)}</span>
                   </>
                 )}
@@ -62,30 +77,52 @@ export default function Comment(props) {
             )}
           </div>
           <div className='Comment_element'>
-            <Typography variant='body1' gutterBottom>
-              {message}
-            </Typography>
+            {!editMode ? (
+              <Typography variant='body1' gutterBottom>
+                {message}
+              </Typography>
+            ) : (
+              <TextField
+                id='Comment'
+                label='Comment'
+                multiline
+                rows={4}
+                variant='outlined'
+                className='NewComment_textfield'
+                value={commentMessage}
+                onChange={(e) => setCommentMessage(e.target.value)}
+              />
+            )}
           </div>
           <div className='Comment_like'>
-            <Button onClick={addLike}>
+            <Button onClick={() => updateLikeDislike('like')}>
               <Icon className='blue' style={{ marginRight: 5 }}>
                 thumb_up
               </Icon>
               {like && like}
             </Button>
-            <Button>
+            <Button onClick={() => updateLikeDislike('dislike')}>
               <Icon className='blue' style={{ marginRight: 5 }}>
                 thumb_down
               </Icon>
               {dislike && dislike}
             </Button>
             <div className='flex1' />
-            <Button size='small' className='Comment_editbutton' onClick={() => {}}>
-              <Icon fontSize={'small'} className='Comment_editbutton_icon'>
+            {editMode ? (
+              <Button size='small' className='Comment_sendbutton' onClick={() => updateMessage()}>
+                <Icon fontSize={'small'} className='Comment_editbutton_icon'>
+                  send
+                </Icon>
+                Send
+              </Button>
+            ) : (
+              <Button size='small' className='Comment_editbutton' onClick={() => setEditMode(true)}>
+                <Icon fontSize={'small'} className='Comment_editbutton_icon'>
+                  edit
+                </Icon>
                 edit
-              </Icon>
-              edit
-            </Button>
+              </Button>
+            )}
           </div>
         </Paper>
       </>
@@ -98,5 +135,5 @@ const getDateFromTimestamp = (timestamp) => {
   const hours = new Date(Date(timestamp)).getHours('fr-FR');
   const minutes = new Date(Date(timestamp)).getMinutes('fr-FR');
 
-  return `${date} - ${hours}:${minutes}`;
+  return `${date} ${hours}:${minutes}`;
 };

1c611b5fd5515aa4d28c6b9238c8802853c98b39
lenasim <radical.rin@gmail.com>
2021-02-04 12:06:02 +0100

    Pass linter but WIP

--- src/Components/Auth/RegisterContainer.js
+++ src/Components/Auth/RegisterContainer.js
@@ -1,11 +1,10 @@
 import React, { useState } from 'react';
-import { Paper } from '@material-ui/core';
 import { useMutation } from '@apollo/client';
 import { REGISTER_USER } from '../../api/User';
 import Register from './Register';
 
 export default function RegisterContainer({ history }) {
-  const [registerUser, { loading }] = useMutation(REGISTER_USER);
+  const [registerUser] = useMutation(REGISTER_USER);
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [ConfirmPassword, setConfirmPassword] = useState('');
@@ -15,37 +14,27 @@ export default function RegisterContainer({ history }) {
   const onEmailHandler = (e) => {
     setEmail(e.currentTarget.value);
   };
-  const onNameHandler = (e) => {
-    setName(e.currentTarget.value);
-  };
   const onPasswordHandler = (e) => {
     setPassword(e.currentTarget.value);
   };
   const onConfirmPasswordHandler = (e) => {
     setConfirmPassword(e.currentTarget.value);
   };
-  const onSubmitHandler = (e) => {
-    e.preventDefault();
-    if (Password !== ConfirmPassword) {
-      return alert('Password does not match');
-    }
-    const body = {
-      email: Email,
-      name: Name,
-      password: Password,
-    };
-    dispatch(registerUser(body)).then((res) =>
-      res.payload.success ? props.history.push('/login') : alert('Failed to sign up')
-    );
+  const onFirstnameHandler = (e) => {
+    setFirstname(e.currentTarget.value);
+  };
+  const onLastnameHandler = (e) => {
+    setLastname(e.currentTarget.value);
   };
 
   const onSubmitHandler = async (e) => {
     e.preventDefault();
-    if (this.state.emailErrorText == '' && this.state.confirmPasswordErrorText == '') {
-      console.log('_onSubmit: state=', this.state);
-    } else {
-      console.log('has error, unable to submit');
-    }
+
+    // if (password !== ConfirmPassword) {
+    //   need to send an error 'Password does not match'
+    // }
+
+    // res.success ? props.history.push('/login') : alert('Failed to sign up')
 
     try {
       const response = await registerUser({
@@ -55,15 +44,28 @@ export default function RegisterContainer({ history }) {
         lastname,
       });
 
+      // eslint-disable-next-line no-console
       console.log(response);
+      history.push('/');
     } catch (err) {
-      console.log(err);
+      // console.log(err);
+      history.push('/login');
     }
-
-    history.push('/');
   };
 
-  return <Register onSubmitHandler={(e) => onSubmitHandler(e)} />;
+  return (
+    <Register
+      onSubmitHandler={(e) => onSubmitHandler(e)}
+      email={email}
+      setEmail={(e) => onEmailHandler(e)}
+      password={password}
+      setPassword={(e) => onPasswordHandler(e)}
+      ConfirmPassword={ConfirmPassword}
+      setConfirmPassword={(e) => onConfirmPasswordHandler(e)}
+      firstname={firstname}
+      setFirstname={(e) => onFirstnameHandler(e)}
+      lastname={lastname}
+      setLastname={(e) => onLastnameHandler(e)}
+    />
+  );
 }
-
-// https://gist.github.com/hsinhoyeh/505e7ae4734350da33903ef3ea68a0b8

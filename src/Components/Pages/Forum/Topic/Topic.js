f9884f37c66ed2b4446636e98425bd4aaacd6712
lenasim <radical.rin@gmail.com>
2021-02-06 22:47:32 +0100

    Refacto - Topic : Split view / logic, solve refetch problem

--- src/Components/Pages/Forum/Topic/Topic.js
+++ src/Components/Pages/Forum/Topic/Topic.js
@@ -1,128 +1,89 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { Paper, Button, Icon, Avatar, Chip, Typography, Link, IconButton } from '@material-ui/core';
-import { useMutation, useQuery } from '@apollo/client';
-import Comment from '../Comment/Comment';
-import NewComment from '../Comment/NewComment';
 import sampleImage from '../../../../images/cat.jpg';
 import getDateFromTimestamp from '../helpers/dates';
-import TopicForm from '../TopicEditor/TopicForm';
-import { ADD_LIKE_TOPIC, GET_LIKES_TOPIC } from '../../../../graphql/Topic';
 import './Topic.css';
 
 const Topic = (props) => {
+  const { data, toggle, setToggle, toggleWrite, setToggleWrite, handleTopicLike, setModifyFormOpened } = props;
   const [heart, setHeart] = useState(false);
-  const [toggle, setToggle] = useState(true);
-  const [toggleWrite, setToggleWrite] = useState(false);
 
-  const { data, refresh } = props;
-  const [modifiyFormOpened, setModifyFormOpened] = useState(false);
-
-  const [addLikeTopic] = useMutation(ADD_LIKE_TOPIC);
-  const { data: fetchedData, refetch } = useQuery(GET_LIKES_TOPIC, { variables: { _id: data._id } });
-
-  const handleTopicLike = () => {
-    refetch();
-    const likes = fetchedData.topic.like + 1;
-    addLikeTopic({ variables: { _id: data._id, like: likes } });
-  };
-
-  useEffect(() => {
-    refresh();
-  }, [modifiyFormOpened, refresh]);
-
-  const sortedComments = Array.from(data.comments);
-
-  let bestComment;
-  if (data.comments.length > 0) {
-    bestComment = data.comments.reduce((prev, current) => (prev.like > current.like ? prev : current));
-
-    const indexBestComment = data.comments.indexOf(bestComment);
-    sortedComments.splice(indexBestComment, 1);
-    sortedComments.unshift(bestComment);
+  if (!data) {
+    return null;
   }
-
   return (
-    <div className='Article'>
-      {modifiyFormOpened ? (
-        <TopicForm
-          mode='update_topic'
-          open={() => setModifyFormOpened(true)}
-          topicData={data}
-          close={() => setModifyFormOpened(false)}
-        />
-      ) : (
-        <Paper className='Article_container' elevation={3}>
-          <div className='Article_header'>
-            <Icon onClick={() => setModifyFormOpened(!modifiyFormOpened)}>create</Icon>
+    <div>
+      <Paper className='Article_container' elevation={3}>
+        <div className='Article_header'>
+          <Icon onClick={() => setModifyFormOpened(true)}>create</Icon>
 
-            <Typography variant='h2'>{data.subject}</Typography>
-            <div className='Article_id'>
-              <Avatar src={sampleImage} />
-              <div className='flex_column' style={{ marginLeft: 10 }}>
-                <Typography variant='button' className='blue'>
-                  {data.username}
-                </Typography>
+          <Typography variant='h2'>{data.subject}</Typography>
+          <div className='Article_id'>
+            <Avatar src={sampleImage} />
+            <div className='flex_column' style={{ marginLeft: 10 }}>
+              <Typography variant='button' className='blue'>
+                {data.username}
+              </Typography>
+              <Typography variant='caption' className='lightgrey'>
+                Posted on
+                <span className='lightgrey bold'> {getDateFromTimestamp(data.date)}</span>
+              </Typography>
+
+              {data.lastUpdateDate && (
                 <Typography variant='caption' className='lightgrey'>
-                  Posted on
-                  <span className='lightgrey bold'> {getDateFromTimestamp(data.date)}</span>
+                  Last update
+                  <span className='lightgrey bold'> {getDateFromTimestamp(data.lastUpdateDate)}</span>
                 </Typography>
-
-                {data.lastUpdateDate && (
-                  <Typography variant='caption' className='lightgrey'>
-                    Last update
-                    <span className='lightgrey bold'> {getDateFromTimestamp(data.lastUpdateDate)}</span>
-                  </Typography>
-                )}
-              </div>
-            </div>
-            <div className='Article_borderline' />
-            <Typography variant='body1' gutterBottom>
-              {data.body}
-            </Typography>
-            {data.url.length > 0 &&
-              data.url.map((u) => (
-                <Link key={u} className='Article_link' href={u} target='_blank' rel='noopener noreferrer'>
-                  <Icon style={{ paddingRight: 10 }}>link</Icon>
-                  {u}
-                </Link>
-              ))}
-            <div className='Article_borderline' />
-            <div className='flex_'>
-              <div>
-                {data.tags.length > 0 &&
-                  data.tags.map((t, idx) => (
-                    <Chip
-                      key={t}
-                      label={t}
-                      variant='outlined'
-                      style={{ marginRight: 5 }}
-                      color={idx % 2 === 0 ? 'primary' : 'secondary'}
-                    />
-                  ))}
-              </div>
-
-              <div style={{ flex: 1 }} />
-              <Button onClick={handleTopicLike}>
-                <Icon className='blue' style={{ marginRight: 5 }}>
-                  thumb_up
-                </Icon>
-                <div className='likes-count'>{fetchedData.like || (data.like ? data.like : '0')}</div>
-              </Button>
-              <Button>
-                <Icon onClick={() => setHeart()} className='red' style={{ marginRight: 5 }}>
-                  {heart ? 'favorite' : 'favorite_border'}
-                </Icon>
-                {heart ? '50' : '49'}
-              </Button>
+              )}
             </div>
           </div>
-        </Paper>
-      )}
+          <div className='Article_borderline' />
+          <Typography variant='body1' gutterBottom>
+            {data.body}
+          </Typography>
+          {data.url?.length > 0 &&
+            data.url.map((u) => (
+              <Link key={u} className='Article_link' href={u} target='_blank' rel='noopener noreferrer'>
+                <Icon style={{ paddingRight: 10 }}>link</Icon>
+                {u}
+              </Link>
+            ))}
+          <div className='Article_borderline' />
+          <div className='flex_'>
+            <div>
+              {data.tags?.length > 0 &&
+                data.tags.map((t, idx) => (
+                  <Chip
+                    key={t}
+                    label={t}
+                    variant='outlined'
+                    style={{ marginRight: 5 }}
+                    color={idx % 2 === 0 ? 'primary' : 'secondary'}
+                  />
+                ))}
+            </div>
+
+            <div style={{ flex: 1 }} />
+            <Button onClick={handleTopicLike}>
+              <Icon className='blue' style={{ marginRight: 5 }}>
+                thumb_up
+              </Icon>
+              <div className='likes-count'>{data.like || '0'}</div>
+            </Button>
+            <Button>
+              <Icon onClick={() => setHeart()} className='red' style={{ marginRight: 5 }}>
+                {heart ? 'favorite' : 'favorite_border'}
+              </Icon>
+              {heart ? '50' : '49'}
+            </Button>
+          </div>
+        </div>
+      </Paper>
 
       <div className='Article_comment_info' elevation={0}>
         <Icon className='blue'>comment</Icon>
         <Typography variant='button' className='blue' style={{ marginLeft: 10 }}>
-          {data.comments.length} Answers
+          {data.comments?.length} Answers
         </Typography>
         <div style={{ flex: 1 }} />
         {!toggleWrite && (
@@ -142,28 +103,6 @@ const Topic = (props) => {
           <Icon className='lightgrey'>{toggle ? 'expand_less' : 'expand_more'}</Icon>{' '}
         </IconButton>
       </div>
-      {toggleWrite && (
-        <NewComment topic_id={data._id} uploaded={() => refresh()} cancel={() => setToggleWrite(false)} />
-      )}
-      {toggle && sortedComments && (
-        <>
-          {sortedComments.map((comment) => (
-            <div key={comment._id}>
-              <Comment
-                commentId={comment._id}
-                name={comment.author}
-                message={comment.commentBody}
-                date={comment.date}
-                like={comment.like}
-                dislike={comment.dislike}
-                lastUpdateDate={comment.lastUpdateDate}
-                refresh={() => refresh()}
-                best={comment._id === bestComment._id ? true : null}
-              />
-            </div>
-          ))}
-        </>
-      )}
     </div>
   );
 };
--- src/Components/Pages/Forum/Topic/TopicContainer.js
+++ src/Components/Pages/Forum/Topic/TopicContainer.js
@@ -1,15 +1,104 @@
-import React from 'react';
-import { useQuery } from '@apollo/client';
+import React, { useState, useEffect } from 'react';
+import { useQuery, useMutation } from '@apollo/client';
+import { GET_TOPIC, ADD_LIKE_TOPIC } from '../../../../graphql/Topic';
 import Topic from './Topic';
-import { GET_TOPIC } from '../../../../graphql/Topic';
+import TopicForm from '../TopicEditor/TopicForm';
+import Comment from '../Comment/Comment';
+import NewComment from '../Comment/NewComment';
 
 function TopicContainer({ match }) {
   const topicId = match.params.id;
-  const { loading, data, refetch } = useQuery(GET_TOPIC, {
+  const { loading, error, data, refetch } = useQuery(GET_TOPIC, {
     variables: { topicId },
   });
+  // eslint-disable-next-line no-unused-vars
+  const [addLikeTopic, { addLikeError, addLikeLoading }] = useMutation(ADD_LIKE_TOPIC, {
+    refetchQueries: [{ query: GET_TOPIC, variables: { topicId } }],
+    awaitRefetchQueries: true,
+  });
+  const [toggle, setToggle] = useState(true);
+  const [toggleWrite, setToggleWrite] = useState(false);
+  const [modifiyFormOpened, setModifyFormOpened] = useState(false);
+  const [bestComment, setBestComment] = useState(null);
+  const [sortedComments, setSortedComments] = useState(null);
 
-  return loading === false && <Topic data={data.topic} refresh={() => refetch()} />;
+  useEffect(() => {
+    if (data?.topic.comments?.length > 0) {
+      const _bestComment = data.topic.comments.reduce((prev, current) => (prev.like > current.like ? prev : current));
+
+      const _sortedComments = Array.from(data.topic.comments);
+      const indexBestComment = data.topic.comments.indexOf(_bestComment);
+      _sortedComments.splice(indexBestComment, 1);
+      _sortedComments.unshift(bestComment);
+
+      setBestComment(_bestComment);
+      setSortedComments(_sortedComments);
+    }
+  }, [bestComment, data]);
+
+  const handleTopicLike = async () => {
+    const likes = data.topic.like + 1;
+    try {
+      addLikeTopic({ variables: { _id: match.params.id, like: likes } });
+    } catch (err) {
+      // eslint-disable-next-line no-console
+      console.err(err, addLikeError);
+    }
+  };
+
+  if (loading) return <p>LOADING...</p>;
+  if (error) return <p>{`ERROR: ${error}`}</p>;
+  return (
+    <>
+      <div className='Article'>
+        <Topic
+          data={data.topic}
+          handleTopicLike={handleTopicLike}
+          toggle={toggle}
+          setToggle={setToggle}
+          toggleWrite={toggleWrite}
+          setToggleWrite={setToggleWrite}
+          setModifyFormOpened={setModifyFormOpened}
+        />
+        {modifiyFormOpened && (
+          <TopicForm
+            mode='update_topic'
+            open={() => setModifyFormOpened(true)}
+            topicData={data.topic}
+            close={() => setModifyFormOpened(false)}
+          />
+        )}
+
+        {toggleWrite && (
+          <NewComment topic_id={data.topic._id} uploaded={() => refetch()} cancel={() => setToggleWrite(false)} />
+        )}
+        {toggle && sortedComments && (
+          <>
+            {sortedComments.map((comment) => {
+              if (!comment) {
+                return null;
+              }
+              return (
+                <div key={comment._id}>
+                  <Comment
+                    commentId={comment._id}
+                    name={comment.author}
+                    message={comment.commentBody}
+                    date={comment.date}
+                    like={comment.like}
+                    dislike={comment.dislike}
+                    lastUpdateDate={comment.lastUpdateDate}
+                    refresh={() => refetch()}
+                    best={comment._id === bestComment._id ? true : null}
+                  />
+                </div>
+              );
+            })}
+          </>
+        )}
+      </div>
+    </>
+  );
 }
 
 export default TopicContainer;

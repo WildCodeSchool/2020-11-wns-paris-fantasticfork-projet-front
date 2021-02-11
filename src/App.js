c94226e94384f910a692b8363a08279a5084d8fd
lenasim <radical.rin@gmail.com>
2021-02-11 20:52:09 +0100

    Feature: protected routes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route, Redirect } from 'react-router-dom';
 import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
 import { Authenticated, Visitor } from './Layouts';
 import RegisterContainer from './Components/Pages/Register/RegisterContainer';
@@ -9,8 +9,7 @@ import ForumContainer from './Components/Pages/Forum/ForumContainer';
 import './App.css';
 
 const client = new ApolloClient({
-  // uri: process.env.REACT_APP_GRAPHQL_URI ||
-  uri: 'http://localhost:4000/graphql',
+  uri: process.env.REACT_APP_GRAPHQL_URI || 'http://localhost:4000/graphql',
   cache: new InMemoryCache(),
   headers: {
     authorization: localStorage.getItem('token') || '',
@@ -22,23 +21,36 @@ function App() {
     <ApolloProvider client={client}>
       <Switch>
         <RouteWrapper exact path='/' component={ForumContainer} layout={Visitor} />
-        <RouteWrapper exact path='/home' component={RegisterContainer} layout={Authenticated} text='home' />
+        <RouteWrapper
+          exact
+          path='/home'
+          component={RegisterContainer}
+          protectedRoute
+          layout={Authenticated}
+          text='Home'
+        />
         <RouteWrapper exact path='/register' component={RegisterContainer} layout={Visitor} />
         <RouteWrapper exact path='/login' component={LoginContainer} layout={Visitor} />
         <RouteWrapper exact path='/topics' history component={ForumContainer} layout={Authenticated} text='Forum' />
-        <RouteWrapper path='/topics/:id' component={TopicContainer} layout={Authenticated} />
+        <RouteWrapper path='/topics/:id' component={TopicContainer} protectedRoute layout={Authenticated} />
       </Switch>
     </ApolloProvider>
   );
 }
 
-function RouteWrapper({ component: Component, layout: Layout, text, ...rest }) {
+function RouteWrapper(props) {
+  const { component: Component, layout: Layout, text, protectedRoute, ...rest } = props;
+  const token = localStorage.getItem('token');
+  if (protectedRoute && !token) {
+    return <Redirect to={{ pathname: '/login' }} />;
+  }
+
   return (
     <Route
       {...rest}
-      render={(props) => (
-        <Layout title={text} {...props}>
-          <Component {...props} />
+      render={(_props) => (
+        <Layout title={text} {..._props}>
+          <Component {..._props} />
         </Layout>
       )}
     />

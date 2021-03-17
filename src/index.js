ea04527ba192b8cfed15457b174510d07b867f64
lenasim <radical.rin@gmail.com>
2021-03-17 21:54:16 +0100

    Refresh token via apollo link working

--- src/index.js
+++ src/index.js
@@ -1,17 +1,63 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
-import { ApolloClient, ApolloLink, HttpLink, ApolloProvider, InMemoryCache, concat } from '@apollo/client';
-import './index.css';
-import App from './App';
+import { ApolloClient, ApolloLink, HttpLink, ApolloProvider, InMemoryCache, from } from '@apollo/client';
+import { TokenRefreshLink } from 'apollo-link-token-refresh';
+// eslint-disable-next-line camelcase
+import jwt_decode from 'jwt-decode';
 import reportWebVitals from './reportWebVitals';
+import App from './App';
+import './index.css';
 
 const httpLink = new HttpLink({
-  uri: process.env.REACT_APP_GRAPHQL_URI || 'http://localhost:4000/graphql',
+  // uri: process.env.REACT_APP_GRAPHQL_URI || 'http://localhost:4000/graphql',
+  uri: 'http://localhost:4000/graphql',
+  credentials: 'include',
+});
+
+const refreshToken = new TokenRefreshLink({
+  accessTokenField: 'token',
+  isTokenValidOrUndefined: () => {
+    const token = localStorage.getItem('stud-connect@token') || '';
+
+    if (!token) {
+      return true;
+    }
+
+    try {
+      const { exp } = jwt_decode(token);
+      if (Date.now() >= exp * 1000) {
+        return false;
+      }
+      return true;
+    } catch {
+      return false;
+    }
+  },
+  fetchAccessToken: () => {
+    const token = localStorage.getItem('stud-connect@token') || null;
+    return fetch('http://localhost:4000/refresh_token' || 'https://stud-connect.herokuapp.com/refresh_token', {
+      method: 'GET',
+      credentials: 'include',
+      headers: {
+        Authorization: `bearer ${token}` || null,
+      },
+    });
+  },
+  handleFetch: (accessToken) => {
+    localStorage.setItem('stud-connect@token', accessToken);
+  },
+  handleError: (err) => {
+    // eslint-disable-next-line no-console
+    console.warn('Your refresh token is invalid. Try to relogin');
+    // eslint-disable-next-line no-console
+    console.error(err);
+  },
 });
 
 const authMiddleware = new ApolloLink((operation, forward) => {
   const token = localStorage.getItem('stud-connect@token') || null;
+
   // add the authorization to the headers
   if (token) {
     operation.setContext({
@@ -24,11 +70,13 @@ const authMiddleware = new ApolloLink((operation, forward) => {
   return forward(operation);
 });
 
-const link = concat(authMiddleware, httpLink);
-
 const client = new ApolloClient({
   cache: new InMemoryCache(),
-  link,
+  // credentials: 'include',
+  fetchOptions: {
+    credentials: 'include',
+  },
+  link: from([refreshToken, authMiddleware, httpLink]),
 });
 
 ReactDOM.render(

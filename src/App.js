d8570d2eaaf520db0cd628ac2e136868b8a93d78
coneeone <nicolas.laudinet@sfr.fr>
2021-03-18 20:42:38 +0100

    set a mui theme, for h6 typography components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
+import { ThemeProvider } from "@material-ui/core";
+import theme from './Components/theme';
 import { Authenticated, Visitor } from './Layouts';
 import RegisterContainer from './Components/Pages/Register/RegisterContainer';
 import LoginContainer from './Components/Pages/Login/LoginContainer';
 import TopicContainer from './Components/Pages/Forum/Topic/TopicContainer';
 import ForumContainer from './Components/Pages/Forum/ForumContainer';
-import './App.css';
+import './App.scss';
 
 const client = new ApolloClient({
   uri: process.env.REACT_APP_GRAPHQL_URI || 'http://localhost:4000/graphql',
@@ -20,12 +22,14 @@ function App() {
   return (
     <ApolloProvider client={client}>
       <Switch>
-        <RouteWrapper exact path='/' component={ForumContainer} layout={Visitor} />
-        <RouteWrapper exact path='/home' component={RegisterContainer} layout={Authenticated} text='home' />
-        <RouteWrapper exact path='/register' component={RegisterContainer} layout={Visitor} />
-        <RouteWrapper exact path='/login' component={LoginContainer} layout={Visitor} />
-        <RouteWrapper exact path='/topics' history component={ForumContainer} layout={Authenticated} text='Forum' />
-        <RouteWrapper path='/topics/:id' component={TopicContainer} layout={Authenticated} />
+        <ThemeProvider theme={theme}>
+          <RouteWrapper exact path='/' component={ForumContainer} layout={Visitor} />
+          <RouteWrapper exact path='/home' component={RegisterContainer} layout={Authenticated} text='home' />
+          <RouteWrapper exact path='/register' component={RegisterContainer} layout={Visitor} />
+          <RouteWrapper exact path='/login' component={LoginContainer} layout={Visitor} />
+          <RouteWrapper exact path='/topics' history component={ForumContainer} layout={Authenticated} text='Forum' />
+          <RouteWrapper path='/topics/:id' component={TopicContainer} layout={Authenticated} />
+        </ThemeProvider>
       </Switch>
     </ApolloProvider>
   );

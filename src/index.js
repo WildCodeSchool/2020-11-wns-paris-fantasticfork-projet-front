0b7385546960714728c58985490137192d3c43e9
lenasim <radical.rin@gmail.com>
2021-04-04 22:36:54 +0200

    change url

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ import './index.css';
 
 const httpLink = new HttpLink({
   // uri: process.env.REACT_APP_GRAPHQL_URI || 'http://localhost:4000/graphql',
-  uri: 'http://localhost:4000/graphql',
+  uri: process.env.REACT_APP_GRAPHQL_URI,
   credentials: 'include',
 });
 

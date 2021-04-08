4009156fbaba07bef006b005886962415bdc2415
lenasim <radical.rin@gmail.com>
2021-04-08 22:35:00 +0200

    Add chat gqls

--- src/graphql/Chat.js
+++ src/graphql/Chat.js
@@ -1,13 +1,53 @@
 import { gql } from '@apollo/client';
 
+export const GET_CHAT_ROOMS = gql`
+  query MyChatRooms($userId: String!) {
+    myChatRooms(userId: $userId) {
+      _id
+      participants {
+        userId
+        lastConnected
+      }
+      messages {
+        userId
+        text
+        createdAt
+      }
+    }
+  }
+`;
+
 // TODO: need to add avatar
-// eslint-disable-next-line import/prefer-default-export
-export const GET_USERS_TO_CREATE_CHAT = gql`
+export const GET_USERS = gql`
   query Users {
     users {
       _id
       firstname
       lastname
+    }
+  }
+`;
+
+export const READ_CHAT = gql`
+  mutation ConnectedToChatRoom($chatRoomId: String!, $userId: String) {
+    connectedToChatRoom(chatRoomId: $chatRoomId, userId: $userId) {
+      _id
+      participants {
+        userId
+        lastConnected
+      }
+    }
+  }
+`;
+
+export const CREATE_CHAT = gql`
+  mutation newChatRoom($participants: [ChatRoomInput!]) {
+    newChatRoom(participants: $participants) {
+      _id
+      participants {
+        userId
+        lastConnected
+      }
     }
   }
 `;

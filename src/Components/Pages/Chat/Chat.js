436590c535e4649581182be2941a94095d0711ab
lenasim <radical.rin@gmail.com>
2021-04-08 22:35:27 +0200

    query:get all chat rooms

--- src/Components/Pages/Chat/Chat.js
+++ src/Components/Pages/Chat/Chat.js
@@ -1,28 +1,43 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import { useQuery } from '@apollo/client';
+import { GET_CHAT_ROOMS } from '../../../graphql/Chat';
 import ChatRoomList from './ChatRoomList';
 import ChatMessages from './ChatMessages';
 import './Chat.scss';
 
 const dummyMessageRooms = [
-  { id: 1, name: 'Dummy Name', lastMessage: 'This is a message', time: '11:50', unread: '2' },
+  { _id: 1, name: 'Dummy Name', lastMessage: 'This is a message', time: '11:50', unread: '2' },
   {
-    id: 2,
+    _id: 2,
     name: 'My Name',
     lastMessage: 'This is a message more than 50 letters. I am long very long',
     time: '18:10',
     unread: '1',
   },
-  { id: 3, name: 'Your Name', lastMessage: 'This is a message33', time: '04/02', unread: null },
-  { id: 4, name: 'Their Name', lastMessage: 'This is a message44', time: '04/01', unread: null },
-  { id: 5, name: 'Our Name', lastMessage: 'This is a message55', time: '03/31', unread: null },
-  { id: 6, name: 'No Name', lastMessage: 'This is a message66', time: '03/15', unread: null },
+  { _id: 3, name: 'Your Name', lastMessage: 'This is a message33', time: '04/02', unread: null },
+  { _id: 4, name: 'Their Name', lastMessage: 'This is a message44', time: '04/01', unread: null },
+  { _id: 5, name: 'Our Name', lastMessage: 'This is a message55', time: '03/31', unread: null },
+  { _id: 6, name: 'No Name', lastMessage: 'This is a message66', time: '03/15', unread: null },
 ];
 
 export default function Chat() {
-  const [selectedRoom, setSelectedRoom] = useState(dummyMessageRooms[0]);
+  const [chatRooms, setChatRooms] = useState(dummyMessageRooms);
+  const [selectedRoom, setSelectedRoom] = useState(chatRooms[0]);
+  const { loading, data } = useQuery(GET_CHAT_ROOMS, { variables: { userId: global.userId } });
+
+  useEffect(() => {
+    if (data?.myChatRooms) {
+      setChatRooms(data.myChatRooms);
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [data, data?.myChatRooms, loading]);
+
+  if (!chatRooms || !selectedRoom) {
+    return null;
+  }
   return (
     <div className='Chat_container'>
-      <ChatRoomList data={dummyMessageRooms} setSelectedRoom={(idx) => setSelectedRoom(dummyMessageRooms[idx])} />
+      <ChatRoomList data={chatRooms} setSelectedRoom={(idx) => setSelectedRoom(dummyMessageRooms[idx])} />
       <ChatMessages data={selectedRoom} />
     </div>
   );

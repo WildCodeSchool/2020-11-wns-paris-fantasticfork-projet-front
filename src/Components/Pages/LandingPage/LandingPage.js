7b23bc39a4cb4ec0caf134c14f59da20b8fec94b
lenasim <radical.rin@gmail.com>
2021-05-06 13:39:24 +0200

    Change text to test CD

--- src/Components/Pages/LandingPage/LandingPage.js
+++ src/Components/Pages/LandingPage/LandingPage.js
@@ -10,7 +10,7 @@ export default function LandingPage({ history }) {
           history.push('/home');
         }}
       >
-        GO TO HOME DEPLOYED
+        GO TO HOME
       </Button>
     </div>
   );

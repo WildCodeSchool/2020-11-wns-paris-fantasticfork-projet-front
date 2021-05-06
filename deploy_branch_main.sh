005924241ee53c70bb756c51b7ce0176c99eee86
lenasim <radical.rin@gmail.com>
2021-05-06 13:48:15 +0200

    remove unnecessary command CD

--- deploy_branch_main.sh
+++ deploy_branch_main.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-git fetch origin && gco ew_deploy && git reset --hard origin/ew_deploy && git clean -f -d
+git fetch origin && git reset --hard origin/ew_deploy && git clean -f -d
 docker-compose -f docker-compose.yml -f docker-compose.prod.yml up --build -d

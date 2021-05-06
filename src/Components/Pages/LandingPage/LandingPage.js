273d112d6621811e6120d589c9b9a43d968f2a8f
lenasim <radical.rin@gmail.com>
2021-05-06 14:22:03 +0200

    test CD

--- src/Components/Pages/LandingPage/LandingPage.js
+++ src/Components/Pages/LandingPage/LandingPage.js
@@ -10,7 +10,7 @@ export default function LandingPage({ history }) {
           history.push('/home');
         }}
       >
-        GO TO HOME EWEW
+        GO TO HOME!!
       </Button>
     </div>
   );

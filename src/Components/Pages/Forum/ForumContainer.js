223001734ea3ff8282f537558eebd48c466773dc
lenasim <radical.rin@gmail.com>
2021-07-16 11:15:26 +0200

    fix forum filter bar css mobile version

--- src/Components/Pages/Forum/ForumContainer.js
+++ src/Components/Pages/Forum/ForumContainer.js
@@ -29,11 +29,11 @@ function ForumContainer({ history }) {
   if (error) return <p>{`ERROR: ${error}`}</p>;
 
   return (
-    <div className='ForumContainer'>
+    <>
       <SearchTools modalOpen={setOpen} topics={{ get: topics, set: setTopics, list: data.topics }}/>
       <Forum topics={topics} goToPage={(TopicId) => goToPage(TopicId)} />
       <TopicForm open={open} close={() => setOpen(false)} />
-    </div>
+    </>
   );
 }
 

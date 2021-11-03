7d9b932d9deb37eeeff653259943380ff518e544
coneeone <nicolas.laudinet@sfr.fr>
2021-11-03 18:20:27 +0100

    modified Forum layout topic list components are now responsive

--- src/Components/Pages/Forum/Forum.js
+++ src/Components/Pages/Forum/Forum.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Avatar, Chip, Icon, Typography, Button, Paper } from '@material-ui/core';
+import { Chip, Icon, Typography, Paper } from '@material-ui/core';
 import getDateFromTimestamp from './helpers/dates';
 import './Forum.scss'
 
@@ -16,21 +16,31 @@ function Forum({ topics, goToPage }) {
               style={{ padding: 30, paddingRight: 50, paddingLeft: 50 }}
               elevation={3}
             >
-              <div className='flex_' style={{ alignItems: 'center', marginBottom: 20 }}>
+              <div className='flex_' style={{ alignItems: 'center'}}>
                 <Typography variant='h5' gutterBottom className='blue'>
                   {topic.subject ? topic.subject : ''}
                 </Typography>
                 <div style={{ flex: 1 }} />
 
-                <Button>
-                  <div style={{ marginRight: 5 }}>{topic.like ? topic.like : '0'}</div>
-                  <Icon className='blue' style={{ marginRight: 15 }}>
-                    thumb_up
-                  </Icon>
-                </Button>
-                <Icon className='blue' style={{ marginRight: 15 }}>
+                {/* <Icon className='blue' style={{ marginRight: 15 }}>
                   share
-                </Icon>
+                </Icon> */}
+              </div>
+
+
+              <div className="row">
+                <div className="author">
+                  {/* <Avatar style={{ width: 30, height: 30, backgroundColor: 'orange', marginRight: 5 }} /> */}
+                  <Typography variant='button' className='blue' style={{ marginRight: 10 }}>
+                    {topic.username}
+                  </Typography>
+                </div>
+
+                <div className="date">
+                  <Typography variant='overline' className='lightgrey'>
+                    {getDateFromTimestamp(topic.createdAt)}
+                  </Typography>
+                </div>
               </div>
 
               <Typography variant='body2' gutterBottom>
@@ -38,27 +48,39 @@ function Forum({ topics, goToPage }) {
               </Typography>
               <Paper elevation={3} />
 
-              <div className='flex_' style={{ alignItems: 'center', marginTop: 20 }}>
-                <Avatar style={{ width: 30, height: 30, backgroundColor: 'orange', marginRight: 5 }} />
-                <Typography variant='button' className='blue' style={{ marginRight: 10 }}>
-                  {topic.username}
-                </Typography>
-                <Typography variant='overline' className='lightgrey'>
-                  {getDateFromTimestamp(topic.createdAt)}
-                </Typography>
-                <div style={{ flex: 1 }} />
-                {topic.tags.length > 0 &&
-                  topic.tags.map((t, idx) => (
-                    <Chip
-                      key={t}
-                      label={t}
-                      variant='outlined'
-                      style={{ marginRight: 5 }}
-                      color={idx % 2 === 0 ? 'primary' : 'secondary'}
-                    />
-                  ))}
+              <div className="topic-data">
+                <div className="tags">
+                  {topic.tags.length > 0 &&
+                    topic.tags.map((t, idx) => (
+                      <Chip
+                        key={t}
+                        label={t}
+                        variant='outlined'
+                        style={{ marginRight: 5 }}
+                        color={idx % 2 === 0 ? 'primary' : 'secondary'}
+                      />
+                    ))}
+                </div>
+                
+
+                <div className="buttons-container">
+                  <div className="buttons">
+                    <div className="like-count">
+                      <div style={{ marginRight: 5 }}>{topic.like ? topic.like : '0'}</div>
+                      <Icon className='blue' style={{ height: '100%' }}>
+                        thumb_up
+                      </Icon>
+                    </div>
+                    <div className="comment-count">
+                      <div>{topic.comments && topic.comments.length}</div>
+                      <div>
+                        <Icon className='blue' style={{ marginLeft: 5, height: '100%', paddingTop: 5 }}>comment</Icon>
+                      </div>
+                    </div>
+                  </div>
+                </div>
               </div>
-              Comments : {topic.comments && topic.comments.length}
+              
             </Paper>
           </div>
         ))}

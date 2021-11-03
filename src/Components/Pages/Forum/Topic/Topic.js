64eed8cd0930931e153c8244904b6b6b20ad0a9a
coneeone <nicolas.laudinet@sfr.fr>
2021-11-03 21:34:11 +0100

    improved the responsiveness of the Topic UI

--- src/Components/Pages/Forum/Topic/Topic.js
+++ src/Components/Pages/Forum/Topic/Topic.js
@@ -84,27 +84,28 @@ const Topic = (props) => {
       </Paper>
 
       <div className='Article_comment_info' elevation={0}>
-        <Icon className='blue'>comment</Icon>
-        <Typography variant='button' className='blue' style={{ marginLeft: 10 }}>
-          {data.comments?.length} Answers
-        </Typography>
-        <div style={{ flex: 1 }} />
-        {!toggleWrite && (
-          <Button
-            className='addComment'
-            style={{ marginRight: 20 }}
-            variant='contained'
-            color='primary'
-            onClick={() => {
-              setToggleWrite(true);
-            }}
-          >
-            Write a comment
-          </Button>
-        )}
-        <IconButton color='primary' onClick={() => setToggle()}>
-          <Icon className='lightgrey'>{toggle ? 'expand_less' : 'expand_more'}</Icon>{' '}
-        </IconButton>
+        <div className="comments-count">
+          <Icon className='blue'>comment</Icon>
+          <Typography variant='button' className='blue' style={{ marginLeft: 10 }}>
+            {data.comments?.length} Answers
+          </Typography>
+        </div>
+        <div className="comment-info-right">
+          {!toggleWrite && ( 
+            <Button
+              className='addComment'
+              variant='contained'
+              color='primary'
+              onClick={() => {
+                setToggleWrite(true);
+              }}
+            >Write a comment</Button>
+          )}
+          <IconButton clasName="toggle-comments" color='primary' onClick={() => setToggle()}>
+            <Icon className='lightgrey'>{toggle ? 'expand_less' : 'expand_more'}</Icon>{' '}
+          </IconButton>
+        </div>
+
       </div>
     </div>
   );
